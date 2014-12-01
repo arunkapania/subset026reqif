c89a80586504cc9009e1a069557fc71b92c64bd1
morido <morido@web.de>
2014-12-01 17:12:14 +0100

    Add chapter8

---
+++ chapter8/media/conversion.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting media conversion. Please stand by.
+
+REM Step1: Convert files
+convert.exe TODO_FIND_A_NAME-8_3_2_3[2]_[f][1]_I.wmf -resize 374x379^! TODO_FIND_A_NAME-8_3_2_3[2]_[f][1]_I.png
+
+REM Step 2: Delete all input files
+del TODO_FIND_A_NAME-8_3_2_3[2]_[f][1]_I.wmf
+echo DONE!
